21332660681c377176f28ae35d72438bd8ff072f
Ricardo Cardenas <cardenas.ricardo@icloud.com>
2018-10-13 21:57:10 +0100

    Coordinates are writing to the console

--- AddressExporter/Form1.Designer.cs
+++ AddressExporter/Form1.Designer.cs
@@ -28,20 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonLoadJSON = new System.Windows.Forms.Button();
+            this.textBoxLoadJSON = new System.Windows.Forms.TextBox();
+            this.buttonExportFolder = new System.Windows.Forms.Button();
+            this.textBoxExportFolder = new System.Windows.Forms.TextBox();
+            this.buttonLoadCSV = new System.Windows.Forms.Button();
+            this.textBoxLoadCSV = new System.Windows.Forms.TextBox();
+            this.dataSetJSON = new System.Data.DataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetJSON)).BeginInit();
             this.SuspendLayout();
+            // 
+            // buttonLoadJSON
+            // 
+            this.buttonLoadJSON.Location = new System.Drawing.Point(12, 12);
+            this.buttonLoadJSON.Name = "buttonLoadJSON";
+            this.buttonLoadJSON.Size = new System.Drawing.Size(150, 23);
+            this.buttonLoadJSON.TabIndex = 0;
+            this.buttonLoadJSON.Text = "Load JSON";
+            this.buttonLoadJSON.UseVisualStyleBackColor = true;
+            this.buttonLoadJSON.Click += new System.EventHandler(this.buttonLoadJSON_Click);
+            // 
+            // textBoxLoadJSON
+            // 
+            this.textBoxLoadJSON.Location = new System.Drawing.Point(168, 14);
+            this.textBoxLoadJSON.Name = "textBoxLoadJSON";
+            this.textBoxLoadJSON.Size = new System.Drawing.Size(500, 20);
+            this.textBoxLoadJSON.TabIndex = 1;
+            // 
+            // buttonExportFolder
+            // 
+            this.buttonExportFolder.Location = new System.Drawing.Point(12, 41);
+            this.buttonExportFolder.Name = "buttonExportFolder";
+            this.buttonExportFolder.Size = new System.Drawing.Size(150, 23);
+            this.buttonExportFolder.TabIndex = 2;
+            this.buttonExportFolder.Text = "Export Folder";
+            this.buttonExportFolder.UseVisualStyleBackColor = true;
+            this.buttonExportFolder.Click += new System.EventHandler(this.buttonExportFolder_Click);
+            // 
+            // textBoxExportFolder
+            // 
+            this.textBoxExportFolder.Location = new System.Drawing.Point(168, 43);
+            this.textBoxExportFolder.Name = "textBoxExportFolder";
+            this.textBoxExportFolder.Size = new System.Drawing.Size(500, 20);
+            this.textBoxExportFolder.TabIndex = 3;
+            // 
+            // buttonLoadCSV
+            // 
+            this.buttonLoadCSV.Location = new System.Drawing.Point(12, 70);
+            this.buttonLoadCSV.Name = "buttonLoadCSV";
+            this.buttonLoadCSV.Size = new System.Drawing.Size(150, 23);
+            this.buttonLoadCSV.TabIndex = 4;
+            this.buttonLoadCSV.Text = "Load CSV";
+            this.buttonLoadCSV.UseVisualStyleBackColor = true;
+            this.buttonLoadCSV.Click += new System.EventHandler(this.buttonLoadCSV_Click);
+            // 
+            // textBoxLoadCSV
+            // 
+            this.textBoxLoadCSV.Location = new System.Drawing.Point(168, 72);
+            this.textBoxLoadCSV.Name = "textBoxLoadCSV";
+            this.textBoxLoadCSV.Size = new System.Drawing.Size(500, 20);
+            this.textBoxLoadCSV.TabIndex = 5;
+            // 
+            // dataSetJSON
+            // 
+            this.dataSetJSON.DataSetName = "DataSetJSON";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(681, 231);
+            this.Controls.Add(this.textBoxLoadCSV);
+            this.Controls.Add(this.buttonLoadCSV);
+            this.Controls.Add(this.textBoxExportFolder);
+            this.Controls.Add(this.buttonExportFolder);
+            this.Controls.Add(this.textBoxLoadJSON);
+            this.Controls.Add(this.buttonLoadJSON);
             this.Name = "Form1";
             this.Text = "Address Exporter";
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetJSON)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonLoadJSON;
+        private System.Windows.Forms.TextBox textBoxLoadJSON;
+        private System.Windows.Forms.Button buttonExportFolder;
+        private System.Windows.Forms.TextBox textBoxExportFolder;
+        private System.Windows.Forms.Button buttonLoadCSV;
+        private System.Windows.Forms.TextBox textBoxLoadCSV;
+        private System.Data.DataSet dataSetJSON;
     }
 }
 
--- AddressExporter/Form1.cs
+++ AddressExporter/Form1.cs
@@ -7,14 +7,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections;
 
 namespace AddressExporter
 {
+    public class PropertiesClass
+    {
+        public string name { get; set; }
+        public object description { get; set; }
+        public string TerritoryType { get; set; }
+        public object TerritoryTypeCode { get; set; }
+        public string TerritoryTypeColor { get; set; }
+        public string TerritoryNumber { get; set; }
+        public object TerritoryNotes { get; set; }
+    }
+
+    public class GeometryClass
+    {
+        public string type { get; set; }
+        public List<List<List<double>>> coordinates { get; set; }
+    }
+
+    public class FeatureClass
+    {
+        public string type { get; set; }
+        public PropertiesClass properties { get; set; }
+        public GeometryClass geometry { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string type { get; set; }
+        public List<FeatureClass> features { get; set; }
+    }
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonLoadJSON_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialogLoadJSON = new OpenFileDialog();
+            openFileDialogLoadJSON.Filter = "JavaScript Object Notation (JSON) | *.json";
+            
+            if (openFileDialogLoadJSON.ShowDialog() == DialogResult.OK)
+            {
+                string strFilename = openFileDialogLoadJSON.FileName;
+                textBoxLoadJSON.Text = strFilename;                
+            }
+
+            //Call JSON function
+            string fileJSON = File.ReadAllText(textBoxLoadJSON.Text);
+            RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(fileJSON);
+
+            int i = 0;
+
+            foreach (var featureCount in rootObject.features)
+            {
+                //Console.WriteLine(i);
+                foreach (var coord in rootObject.features[i].geometry.coordinates[0])
+                {
+                    Console.WriteLine("{0},{1}", coord[0], coord[1]);
+                }
+                i++;
+            }
+
+            
+        }
+
+        private void buttonExportFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialogExportFolder = new FolderBrowserDialog();
+
+            if (folderBrowserDialogExportFolder.ShowDialog() == DialogResult.OK)
+            {
+                string strFoldername = folderBrowserDialogExportFolder.SelectedPath;
+                textBoxExportFolder.Text = strFoldername;
+            }
+        }
+
+        private void buttonLoadCSV_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialogLoadCSV = new OpenFileDialog();
+            openFileDialogLoadCSV.Filter = "Comma Separated Value (CSV) | *.csv";
+
+            if(openFileDialogLoadCSV.ShowDialog() == DialogResult.OK)
+            {
+                string strFilename = openFileDialogLoadCSV.FileName;
+                textBoxLoadCSV.Text = strFilename;
+            }
+
+            //Call CSV function
         }
     }
 }
